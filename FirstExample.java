01d3d4952776f0513c0c15ee12197a364c20a06a
Meena Kabduwal <kabduwalmeena@gmail.com>
2021-02-17 21:31:35 +0100

    Completed the project

--- FirstExample.java
+++ FirstExample.java
@@ -1,6 +1,9 @@
 public class FirstExample{
     public static void main(String[] args){
         int n = 1;
-        System.out.println("N\t 10*N  \t100*N  \t1000N");
+        System.out.println("N\t10*N  \t100*N  \t1000N");
+        for (n = 1; n <= 5; n++){
+            System.out.println(n + " \t" + 10*n + "  \t"+ 100*n +"  \t" + 1000*n);
+        }
     }
 }
