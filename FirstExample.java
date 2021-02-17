30d1789d9023ad868a892f15557f80221fc61a35
Meena Kabduwal <kabduwalmeena@gmail.com>
2021-02-17 21:17:25 +0100

    created a variable

--- FirstExample.java
+++ FirstExample.java
@@ -1,3 +1,6 @@
-public class firstExample{
-    public static void main(String[])
+public class FirstExample{
+    public static void main(String[] args){
+        int n = 1;
+        System.out.println("N\t 10*N  \t100*N  \t1000N");
+    }
 }
